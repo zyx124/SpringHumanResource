0d38999708d2830ea4024064f52b7f95427da2ca
zyx124 <zhaoyuxin124@gmail.com>
2020-05-25 17:02:33 -0400

    initialize the package

---
+++ src/main/java/com/example/humanresource/HumanresourceApplication.java
@@ -0,0 +1,13 @@
+package com.example.humanresource;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HumanresourceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HumanresourceApplication.class, args);
+	}
+
+}
