5504d77dbaeb2f45b04df61e2634e88208f7f74d
zyx124 <zhaoyuxin124@gmail.com>
2020-06-28 20:39:51 -0400

    modify controller functions

--- src/main/java/com/example/humanresource/controller/DepartmentFrontController.java
+++ src/main/java/com/example/humanresource/controller/DepartmentFrontController.java
@@ -7,9 +7,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.*;
 
 import javax.naming.Binding;
 import javax.validation.Valid;
@@ -78,4 +76,6 @@ public class DepartmentFrontController {
         model.addAttribute("departments", departments);
         return "show-department";
     }
+
+
 }
